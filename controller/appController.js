0b28df1f3624a0c0938fcf5e22adfa47ff3daaf4
Risheet Parmar <parmarrisheet29@gmail.com>
2023-07-24 16:01:00 +0530

    Added mailgen to generate mail

--- controller/appController.js
+++ controller/appController.js
@@ -1,11 +1,101 @@
-export function signup(req, res) {
-  res.status(201).send({
-    msg: "Signup successful...",
+import * as nodemailer from "nodemailer";
+import env from "../config.js";
+import Mailgen from "mailgen";
+
+// send mail from testing account
+// didn' really work .. said some paid shit so let's just make our email
+export async function signup(req, res) {
+  // Generate test SMTP service account from ethereal.email
+  // only needed if you don't  have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using Default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: "smtp.forwardemail.net",
+    port: 465,
+    secure: true,
+    auth: {
+      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
   });
+
+  let msg = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  };
+
+  transporter
+    .sendMail(msg)
+    .then(() => {
+      return res.status(201).json({
+        msg: "mail sent successfully",
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({ err });
+    });
 }
 
 export function getBill(req, res) {
-  res.status(201).send({
-    msg: "getBill successful...",
+  const { userEmail } = req.body;
+
+  let config = {
+    service: "gmail",
+    auth: {
+      user: env.EMAIL,
+      pass: env.PASSWORD,
+    },
+  };
+
+  let transporter = nodemailer.createTransport(config);
+
+  let mailgenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "mailgen",
+      link: "https://google.com",
+    },
   });
+
+  let mailBody = {
+    body: {
+      name: "Parmar Risheet",
+      intro: "bill arrived",
+      table: {
+        data: {
+          item: "Nodemailer",
+          description: "A backend stack application",
+          price: "$10.99",
+        },
+      },
+      outro: "Looking forward to do more business",
+    },
+  };
+
+  // to generate the mail from mailBody variable
+  // that must be the syntax to generate the email...
+  let mail = mailgenerator.generate(mailBody);
+
+  let message = {
+    from: env.EMAIL,
+    to: userEmail,
+    subject: "Place order",
+    html: mail,
+  };
+
+  transporter
+    .sendMail(message)
+    .then(() => {
+      return res.status(201).json({
+        msg: "Mail sent successfully...",
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({ err });
+    });
 }
